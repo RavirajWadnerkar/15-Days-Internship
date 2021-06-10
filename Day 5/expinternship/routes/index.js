9163709ebf18518f49b9336bac84cc609f2c623a
Raviraj Wadnerkar <ravirajwadnerkar@gmail.com>
2021-06-10 20:08:30 +0530

    made multiple pages website using node

--- Day 5/expinternship/routes/index.js
+++ Day 5/expinternship/routes/index.js
@@ -23,4 +23,31 @@ router.get('/master', function(req, res, next) {
   res.render('master');
 });
 
+router.get('/form', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/form-process', function(req, res, next) {
+  
+  var name = req.body.text1;
+  var age = parseInt(req.body.text2);
+  var a= parseInt(req.body.text3);
+  var b= parseInt(req.body.text4);
+  var c = a + b;
+  var msg = "";
+  var mycolor = "";
+  if(age>18)
+  {
+    msg = "Eligible for driving License";
+    mycolor = "green";
+  }
+  else{
+    msg = "Not Eligible for driving license";
+    mycolor = "Red";
+  }
+  console.log(req.body);
+  console.log("Name: " + a + "Age:" +b);
+  res.render('ans',{name:name,age:age,a:a,b:b,c:c,msg:msg,mycolor:mycolor});
+});
+
 module.exports = router;
